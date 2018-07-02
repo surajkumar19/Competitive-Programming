f97e1b7ed6f67de376d35fc5981eadba0a57ce6d
surajkumar19 <devarakondasuraj19@gmail.com>
2018-07-02 10:13:56 +0530

    Add files via upload

---
+++ week2/DeleteNode.py
@@ -0,0 +1,74 @@
+# O(1)
+import unittest
+
+
+def delete_node(node_to_delete):
+    # Delete the input node from the linked list
+    '''
+    if(node_to_delete.next is None or node_to_delete is None):
+        assert (), "Exception"
+
+    while node_to_delete.next is not None:
+        node_to_delete.value = node_to_delete.next.value
+        if( node_to_delete.next.next is None):
+            break
+        node_to_delete = node_to_delete.next
+    node_to_delete.next = None
+    '''
+
+    x = node_to_delete.next
+    if x:
+        node_to_delete.next = x.next
+        node_to_delete.value = x.value
+
+    else:
+        assert (), "Exception"
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+    class LinkedListNode(object):
+        def __init__(self, value, next=None):
+            self.value = value
+            self.next = next
+
+        def get_values(self):
+            node = self
+            values = []
+            while node is not None:
+                values.append(node.value)
+                node = node.next
+            return values
+
+    def setUp(self):
+        self.fourth = Test.LinkedListNode(4)
+        self.third = Test.LinkedListNode(3, self.fourth)
+        self.second = Test.LinkedListNode(2, self.third)
+        self.first = Test.LinkedListNode(1, self.second)
+
+    def test_node_at_beginning(self):
+        delete_node(self.first)
+        actual = self.first.get_values()
+        expected = [2, 3, 4]
+        self.assertEqual(actual, expected)
+
+    def test_node_in_middle(self):
+        delete_node(self.second)
+        actual = self.first.get_values()
+        expected = [1, 3, 4]
+        self.assertEqual(actual, expected)
+
+    def test_node_at_end(self):
+        with self.assertRaises(Exception):
+            delete_node(self.fourth)
+
+    def test_one_node_in_list(self):
+        unique = Test.LinkedListNode(1)
+        with self.assertRaises(Exception):
+            delete_node(unique)
+
+
+unittest.main(verbosity=2)
