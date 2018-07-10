28a13a5a9c82685ac26ed6248e499fcd39755991
surajkumar19 <devarakondasuraj19@gmail.com>
2018-07-10 11:29:51 +0530

    Add files via upload

---
+++ week3/day2/Find_Repeat.py
@@ -0,0 +1,63 @@
+# O(nlgn) time and O(1) space.
+import unittest
+
+def find_repeat(the_list):
+    floor = 1
+    ceiling = len(the_list) - 1
+
+    while floor < ceiling:
+        midpoint = floor + ((ceiling - floor) // 2)
+        lower_range_floor, lower_range_ceiling = floor, midpoint
+        upper_range_floor, upper_range_ceiling = midpoint+1, ceiling
+
+        items_in_lower_range = 0
+        for item in the_list:
+
+            if lower_range_floor <= item <= lower_range_ceiling:
+                items_in_lower_range += 1
+
+        distinct_possible_integers_in_lower_range = (
+            lower_range_ceiling
+            - lower_range_floor
+            + 1
+        )
+        if items_in_lower_range > distinct_possible_integers_in_lower_range:
+
+            floor, ceiling = lower_range_floor, lower_range_ceiling
+        else:
+
+            floor, ceiling = upper_range_floor, upper_range_ceiling
+
+    return floor
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_just_the_repeated_number(self):
+        actual = find_repeat([1, 1])
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_short_list(self):
+        actual = find_repeat([1, 2, 3, 2])
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_medium_list(self):
+        actual = find_repeat([1, 2, 5, 5, 5, 5])
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_long_list(self):
+        actual = find_repeat([4, 1, 4, 8, 3, 2, 7, 6, 5])
+        expected = 4
+        self.assertEqual(actual, expected)
+
+
+unittest.main(verbosity=2)
